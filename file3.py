807326f93094011e9598f36b24ea790d287ac67d
consigoclaire <21-72097@g.batstate-u.edu.ph>
2024-07-05 11:15:56 +0800

    Added Circle class with area and circumference calculation to

---
+++ file3.py
@@ -0,0 +1,14 @@
+import math
+class Circle:
+ def __init__(self, radius):
+ self.radius = radius
+
+ def calculate_area(self):
+ return math.pi * self.radius ** 2
+
+ def calculate_circumference(self):
+ return 2 * math.pi * self.radius
+circle = Circle(5)
+print(f"Area of the circle: {circle.calculate_area()}")
+print(f"Circumference of the circle:
+{circle.calculate_circumference()}")
